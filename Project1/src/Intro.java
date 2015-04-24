3632fb43ca0e3c65b2341786473ceaea0d9ca11e
Wayne-S <waynestewart95@gmail.com>
2015-04-24 15:57:18 -0400

    Anything

---
+++ Project1/src/Intro.java
@@ -0,0 +1,14 @@
+
+public class Intro {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello! My name is Wayne.");
+		//adding the commit here
+		/*another commit
+		 * last
+		 */
+
+	}
+
+}
