217731bc08ac6feecccb46302e2e6a0bd1332e7e
Wayne-S <waynestewart95@gmail.com>
2015-04-29 13:42:17 -0400

    Rectangle Area and Circle

---
+++ CalculateArea.java
@@ -0,0 +1,25 @@
+
+public class CalculateArea 
+{
+
+	public static void main(String[] args) {
+		// Finding Area of circle and Rectangle
+		double circleArea= CalculatecircleArea (2)
+		System.out.println(" The area of the circle is :  " + circleArea)
+		double rectangleArea= (5,7)
+		System.out.println(" The area of the rectangle is :  " + rectangleArea)
+	}
+	
+	static double CalculatecircleArea(int radius)
+	{
+		double area = 3.14 * radius * radius;
+		return area;
+	} 
+	static double = CalculateRectangleArea(int length,int breadth)
+	{
+		double area = length * breadth;
+		return area;
+
+	}
+
+}
