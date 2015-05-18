e0d148a182e51251b937bbd7b385ad898d265b64
Wayne-S <waynestewart95@gmail.com>
2015-05-17 23:23:22 -0400

    Account.java

---
+++ Account/src/Account.java
@@ -0,0 +1,64 @@
+
+public class Account 	
+{
+	int m_accountId = -1;
+	private String m_accountName;
+	private int m_customerId = -1;
+	double m_amount = 0.0;
+
+
+public int GetAccountId()
+{
+	return m_accountId;
+}
+
+private void SetAccountId(int accountId)
+{
+	m_accountId = accountId;
+}
+
+public String GetAccountName()
+{
+	return m_accountName;
+}
+
+private void SetAccountName(String accountName)
+{
+	m_accountName = accountName;
+}
+
+
+	public Account(int accountId, String accountName, int customerId)
+	{
+		SetAccountId(accountId);
+		SetAccountName(accountName);
+		SetCustomerId(customerId);
+	}
+
+	private void SetCustomerId(int customerId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private  void UpdateAmount(double newAmount)
+	{
+		m_amount = newAmount;
+	}
+
+	public void UpdateAmountSecurely(double newAmount, String password)
+	{
+		if (password != null) //always do a null check
+		{
+			if (password.equals("admin"))
+			{
+				UpdateAmount(newAmount);
+System.out.println("Updated amount in account id " + m_accountId + " to " + m_amount);
+			}
+			else
+			{
+				System.out.println("Call the police! Unsecure access!!");
+			}
+		}
+	}
+}
+
