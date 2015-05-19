bb99f535506e91cd80a75059414de0df5b07d1e3
Wayne-S <waynestewart95@gmail.com>
2015-05-19 13:10:17 -0400

    DayofWeek.java

--- DayOfWeek/DayOfWeek/src/DayOfWeek.java
+++ DayOfWeek/DayOfWeek/src/DayOfWeek.java
@@ -19,24 +19,46 @@ public class DayOfWeek {
 	{
 		if (dayOfWeek != null)
 		{
-			if 	(dayOfWeek.equalsIgnoreCase("Saturday")
-				||dayOfWeek.equalsIgnoreCase("Saturday"))
-				{
-				System.out.println(dayOfWeek + " Is the Weekend!!!!");
-				}
-			else if (dayOfWeek.equalsIgnoreCase("Monday")
-					||dayOfWeek.equalsIgnoreCase("Tuesday")
-					||dayOfWeek.equalsIgnoreCase("Wednesday")
-					||dayOfWeek.equalsIgnoreCase("Thursday")
-					||dayOfWeek.equalsIgnoreCase("Friday"))
+			String lowerCaseDayOfWeek = dayOfWeek.toLowerCase();
+			
+			switch(lowerCaseDayOfWeek)
 			{
-				System.out.println(dayOfWeek + " is a weekday, I have to go to the store");		
+			case("Monday"):
+			case("Tuesday"):
+			case("Wednesday"):
+			case("Thursday"):
+			case("Friday"):
+			{
+				System.out.println(" It is a weekday, I have to go to work  : ");
+				break;
 			}
-			else
+			case("Saturday"):
+			case("Sunday"):
 			{
-				System.out.println(dayOfWeek + "is not even a valid day!!");
+				System.out.println("I is a weekend,TURN UP");
+				break;
+			}
+				
+			
+			}
+			}
+			//if 	(dayOfWeek.equalsIgnoreCase("Saturday")
+				//||dayOfWeek.equalsIgnoreCase("Saturday"))
+				{
+				//System.out.println(dayOfWeek + " Is the Weekend!!!!");
+				}
+			//else if (dayOfWeek.equalsIgnoreCase("Monday")
+			//		||dayOfWeek.equalsIgnoreCase("Tuesday")
+			//		||dayOfWeek.equalsIgnoreCase("Wednesday")
+			//		||dayOfWeek.equalsIgnoreCase("Thursday")
+			//		||dayOfWeek.equalsIgnoreCase("Friday"))
+			{
+				//System.out.println(dayOfWeek + " is a weekday, I have to go to the store");		
+			}
+			//else
+			{
+			//	System.out.println(dayOfWeek + "is not even a valid day!!");
 			}
 		}
 }
 
-}
