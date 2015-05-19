24a2706ee0830d1f9dd8868884e07d5fa3596c61
Wayne-S <waynestewart95@gmail.com>
2015-05-19 09:52:46 -0400

    DayOfWeek.java

---
+++ DayOfWeek/DayOfWeek/src/DayOfWeek.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class DayOfWeek {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a new day of week:\n");
+		String day = scanner.next();
+		System.out.println("I read this day of week " + day);
+		
+		DayOfWeek dayOfWeekEvaluator = new DayOfWeek();
+		dayOfWeekEvaluator.EvaluateDayOfWeek(day);
+		 
+		scanner.close();
+	}
+
+	void EvaluateDayOfWeek(String dayOfWeek)
+	{
+		if (dayOfWeek != null)
+		{
+			if 	(dayOfWeek.equalsIgnoreCase("Saturday")
+				||dayOfWeek.equalsIgnoreCase("Saturday"))
+				{
+				System.out.println(dayOfWeek + " Is the Weekend!!!!");
+				}
+			else if (dayOfWeek.equalsIgnoreCase("Monday")
+					||dayOfWeek.equalsIgnoreCase("Tuesday")
+					||dayOfWeek.equalsIgnoreCase("Wednesday")
+					||dayOfWeek.equalsIgnoreCase("Thursday")
+					||dayOfWeek.equalsIgnoreCase("Friday"))
+			{
+				System.out.println(dayOfWeek + " is a weekday, I have to go to the store");		
+			}
+			else
+			{
+				System.out.println(dayOfWeek + "is not even a valid day!!");
+			}
+		}
+}
+
+}
